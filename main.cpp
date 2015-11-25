77278e95f1b02757a0f0601264605ecc576a0f10
Jinyue Song <jinyuesong88@gmail.com>
2015-11-25 16:52:10 -0500

    Rock and Roll
    …

---
+++ main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  Ray
+//
+//  Created by Jinyue Song on 2015-11-25.
+//  Copyright © 2015 Jinyue Song. All rights reserved.
+//
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <iostream>
+
+#ifdef __APPLE__
+#   include <OpenGL/gl.h>
+#   include <OpenGL/glu.h>
+#   include <GLUT/glut.h>
+#else
+#   include <GL/gl.h>
+#   include <GL/glu.h>
+#   include <GL/freeglut.h>
+#endif
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
